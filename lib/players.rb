4a732f2a1bcc586dadaea6ab01108e66985ba191
Joseph Burke <joseph_burke@outlook.com>
2020-08-07 14:06:46 +0100

    Make changes to players.rb in accordance with rubocop

--- lib/players.rb
+++ lib/players.rb
@@ -1,25 +1,24 @@
 class Player
-    attr_reader :symbol
-    attr_accessor :name, :order
-    def initialize(name)
-        @name = name
-        @symbol = nil
-        @order = nil
-    end
-    
-    def take_opposite_symbol_to(other_player)
-        case other_player.symbol
-        when 'O'
-            self.symbol = 'X'
-        when 'X'
-            self.symbol = 'O'
-        end
-    end
+  attr_reader :symbol
+  attr_accessor :name, :order
+  def initialize(name)
+    @name = name
+    @symbol = nil
+    @order = nil
+  end
 
-    def symbol=(arg)
-        @symbol = arg
-        @order = 1 if @symbol == 'O'
-        @order = 2 if @symbol == 'X'
+  def take_opposite_symbol_to(other_player)
+    case other_player.symbol
+    when 'O'
+      self.symbol = 'X'
+    when 'X'
+      self.symbol = 'O'
     end
+  end
 
+  def symbol=(arg)
+    @symbol = arg
+    @order = 1 if @symbol == 'O'
+    @order = 2 if @symbol == 'X'
+  end
 end
