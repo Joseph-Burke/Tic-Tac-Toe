cd6c09c8efb7b2c325372c63bbae3ef80bf93205
Joseph Burke <joseph_burke@outlook.com>
2020-08-07 15:12:02 +0100

    Create #empty_line method to add space between text

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 board = [
   '         |         |         ',
   '         |         |         ',
@@ -18,18 +20,26 @@ board = [
   '         |         |         '
 ]
 
-puts 'Welcome to Tic-Tac-Toe'
+def empty_line(num = 1)
+  puts "\n" * num
+end
+
+empty_line
+puts 'Welcome to Tic-Tac-Toe!'
+empty_line(2)
 
 puts 'Player 1, enter your name: '
 player_1_name = gets.chomp.capitalize
+empty_line
 puts 'Player 2, enter your name: '
 player_2_name = gets.chomp.capitalize
 
 player_1_symbol = nil
 
 while player_1_symbol.nil?
-
+  empty_line
   puts "#{player_1_name}, choose a symbol. X or O?"
+  empty_line
 
   input = gets.chomp.strip.upcase
   if %w[X O].include?(input)
@@ -41,10 +51,12 @@ end
 
 player_2_symbol = (player_1_symbol == 'X' ? 'O' : 'X')
 
+empty_line
 puts [
   "#{player_1_name} is #{player_1_symbol}.",
   "#{player_2_name} is #{player_2_symbol}."
 ]
+empty_line
 
 puts board
 
@@ -57,27 +69,31 @@ while turn_counter < 9
   input = nil
 
   until (1..9).include?(input) && available_tiles.include?(input)
-
+    empty_line
     puts "It's your turn, #{current_player}. Enter a number between 1 and 9 to place your symbol on an empty tile."
+    empty_line
     print 'The following tiles are available: '
     available_tiles.each { |e| print " #{e} " }
-    puts "\n\n"
+    empty_line(2)
     input = gets.chomp.to_i
 
     unless (1..9).include?(input)
+      empty_line
       puts 'Invalid input! Enter a number between 1 and 9.'
       next
     end
 
-    puts 'Invalid input! That tile has already been taken.' unless available_tiles.include?(input)
+    puts "\nInvalid input! That tile has already been taken." unless available_tiles.include?(input)
   end
 
   available_tiles.delete(input)
 
   puts board
+  empty_line
 
   turn_counter += 1
 end
 
 winner = players[rand(0..1)]
 puts "#{winner} wins! Well played!"
+empty_line
